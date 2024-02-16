124e751b8d86c62a392c391f5d2ea19d500bab86
Dakota Lust <lustdakota14@gmail.com>
2024-02-15 17:24:37 -0700

    questions submit, get request to movies api, App state updated with moviesList

--- src/components/Questionaire.jsx
+++ src/components/Questionaire.jsx
@@ -1,47 +1,72 @@
 import React from 'react'
 import { Model, Survey } from 'survey-react-ui'
 import 'survey-core/defaultV2.min.css'
+import axios from 'axios'
 
 const questions = {
-    "elements": [
+    'elements': [
         {
-            "name": "Streaming services",
-            "title": "Select all streaming services to search",
-            "type": "tagbox",
-            "choices": [
+            'name': 'Streaming services',
+            'title': 'Select all streaming services to search',
+            'type': 'tagbox',
+            'choices': [
                 { 
-                    "text": "Netflix",
-                    "value": "Netflix"
+                    'text': 'Netflix',
+                    'value': 'netflix'
                 },
                 {
-                    "text": "Hulu",
-                    "value": "Hulu"
+                    'text': 'Hulu',
+                    'value': 'hulu.subscription'
                 },
                 {
-                    "text": "Prime Video",
-                    "value": "Prime Video"
+                    'text': 'Prime Video',
+                    'value': 'prime.subscription'
                 },
                 {
-                    "text": "Disney+",
-                    "value": "Disney+"
+                    'text': 'Disney+',
+                    'value': 'disney'
+                },
+                {
+                    'text': 'HBO Max',
+                    'value': 'hbo'
+                },
+                {
+                    'text': 'Peacock',
+                    'value': 'peacock.subscription'
                 }
             ],
-            "isRequired": true,
-            "hideSelectedItems": true
+            'isRequired': true,
+            'hideSelectedItems': true
         },
         {
-            "name": "Genre(s)",
-            "title": "Select movie genre",
-            "type": "dropdown",
-            "choices": [ "Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Romance", "Thriller", "Science Fiction" ],
-            "isRequired": true
+            'name': 'Genre(s)',
+            'title': 'Select movie genre',
+            'type': 'dropdown',
+            'choices': [ 'Action', 'Adventure', 'Animation', 'Comedy', 'Drama', 'Documentary', 'Fantasy', 'Family', 'Horror', 'History', 'Mystery', 'Music', 'Romance', 'Science Fiction', 'Thriller', 'War' ],
+            'isRequired': true
         }
     ],
-    "showQuestionNumbers": false
+    'showQuestionNumbers': false
 }
 
-function Questionaire () {
+function Questionaire (props) {
     const questionaire = new Model(questions)
+
+    questionaire.onComplete.add((sender) => {
+        const services = sender.data['Streaming services'].join(',')
+        const genres = sender.data['Genre(s)']
+
+        axios.get(`http://localhost:3000/getMovies?services=${services}&genres=${genres}`)
+        .then((response) => {
+            console.log('Success!');
+            //function from App through props to update movies list
+            props.updateMoviesList(response.data)
+        })
+        .catch((err) => {
+            throw new Error(err)
+        })
+    })
+
     return (<Survey model={questionaire} />)
 }
 
