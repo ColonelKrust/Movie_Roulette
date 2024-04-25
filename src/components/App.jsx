f7491d03e53485a3b0f59f7fab90bddfd775400f
Dakota Lust <lustdakota14@gmail.com>
2024-04-24 20:41:25 -0600

    hamburger sidebar button created and styled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Questionnaire from './Questionnaire.jsx'
 import RouletteWheel from './RouletteWheel.jsx'
 import rouletteImage from '../../images/rouletteWheel.png'
+import Button from 'react-bootstrap/Button'
+import Offcanvas from 'react-bootstrap/Offcanvas'
+import { List, Film } from 'react-bootstrap-icons'
 import '../styles/appStyle.css'
 
 class App extends React.Component {
@@ -9,17 +12,28 @@ class App extends React.Component {
     super(props)
     this.state = {
       moviesList: [],
-      chosenMovie: {}
+      chosenMovie: {},
+      show: false
     }
 
-    this.updateMoviesList = this.updateMoviesList.bind(this);
+    this.updateMoviesList = this.updateMoviesList.bind(this)
+    this.showSidebar = this.showSidebar.bind(this)
+    this.hideSidebar = this.hideSidebar.bind(this)
   }
 
   updateMoviesList(moviesArray) {
     this.setState({
       moviesList: moviesArray,
       chosenMovie: moviesArray[Math.floor(Math.random() * moviesArray.length)]
-    });
+    })
+  }
+
+  showSidebar() {
+    this.setState({ show: true })
+  }
+
+  hideSidebar() {
+    this.setState({ show: false })
   }
 
   render() {
@@ -29,6 +43,7 @@ class App extends React.Component {
           <h1 id='appTitle'>Movie Roulette</h1>
           <img id='rouletteLogo' src={rouletteImage} alt='Roulette wheel image' />
           <p id='appDesc'>Put a spin on movie night</p>
+          <Button id='sidebarButton' onClick={this.showSidebar}><List size={40} color='#eaeaea'/></Button>
         </div>
         <Questionnaire updateMoviesList={(array) => this.updateMoviesList(array)}/>
         <br></br>
